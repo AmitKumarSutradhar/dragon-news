fde4a5b890ca97c5df14d327e3018cc80dde12ca
Amit Kumar Sutradhar <dev.amitkumarsutradhar@gmail.com>
2024-11-18 11:14:55 +0600

    Implement private route

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -8,22 +8,27 @@ const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
 
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
     
-    console.log(user);
+    console.log(loading, user);
 
     const createNewUser = (email, password) =>{
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
     const userLogin = (email, password) =>{
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
 
     const logOut = () => {
+        setLoading(true);
         signOut(auth);
     }
 
     const authInfo = {
+        loading,
         user, 
         setUser,
         createNewUser,
@@ -32,8 +37,9 @@ const AuthProvider = ({ children }) => {
     }
 
     useEffect(() =>{
-        const unsubscribe = onAuthStateChanged(auth, currentUser=> {
-            setUser(currentUser)
+        const unsubscribe = onAuthStateChanged(auth, (currentUser)=> {
+            setUser(currentUser);
+            setLoading(false);
         });
 
         return ()=> {
